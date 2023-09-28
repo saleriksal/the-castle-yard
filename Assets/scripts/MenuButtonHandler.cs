32c6d17cf90e64c16ca772a5a4626082f06f2ecd
EliasNieminen06 <143606682+EliasNieminen06@users.noreply.github.com>
2023-09-28 09:11:47 +0300

    menu screen

--- Assets/scripts/MenuButtonHandler.cs
+++ Assets/scripts/MenuButtonHandler.cs
@@ -2,11 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtonHandler : MonoBehaviour
 {
+    public Image jumpScare;
     public void PlayButton()
     {
         SceneManager.LoadScene("GameMainScene");
+    }
+    public void JumpScare()
+    {
+        jumpScare.enabled = true;
+        StartCoroutine(FadeImage(true));
+    }
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                jumpScare.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                jumpScare.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
     }
 }
